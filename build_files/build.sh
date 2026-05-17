e883aaf8628200efe01145251dca99a4ac8f52bd
Paweł Zatoka <github@hoxi.me>
2026-05-17 05:08:53 +0200

    upload changes

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
+dnf5 install -y koji
+koji download-build --arch=x86_64 --arch=noarch grub2-2.12-55.fc44
+dnf5 install -y ./*.rpm --allow-downgrade
 
 # Use a COPR Example:
 #
@@ -21,4 +24,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
